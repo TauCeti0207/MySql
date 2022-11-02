e3f96754cb6fbcce312933f3855be0237a54951d
TauCeti0207 <2076188013@qq.com>
2022-11-02 17:13:51 +0800

    mysql

--- 7-24day01/01.sql
+++ 7-24day01/01.sql
@@ -1,3 +1,4 @@
 show databases;
 show databases;
 show databases;
+show databases;
--- 7-24day01/02.sql
+++ 7-24day01/02.sql
@@ -1,5 +1,6 @@
 SHOW DATABASES;
-USE phpmyadmin;
+# USE phpmyadmin;
+use phpmyadmin; #可以大写可以小写
 SHOW TABLES;
 #SHOW TABLES; #表示单行注释
 DESC pma__users;
